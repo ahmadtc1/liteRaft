2a751f881dabce07d2b51b4fc94bd7a31bb58e94
ahmadtc1 <ahmadtc2000@gmail.com>
2021-04-18 22:37:23 -0400

    Adding some more structures and implementing some basic CM methods
    defining structures for RequestVote and AppendEntries RPCs which will be implemented later on. Will use these structs to pass data between servers
    
    Adding NewConsensusModule method to create a new consensus module
    Adding debugLog method to help with debugging and be able to toggle debug messages
    Adding Report method for CM to report on its current status
    Adding Stop method to stop CM

--- raft.go
+++ raft.go
@@ -1,9 +1,13 @@
 package liteRaft
 
 import (
+	"fmt"
+	"log"
 	"sync"
 	"time"
 )
+
+const debugEnable = 1
 
 type CMState int
 
@@ -54,4 +58,70 @@ type ConsensusModule struct {
 	// volatile raft state on all servers
 	state              CMState
 	electionResetEvent time.Time
+}
+
+func NewConsensusModule(id int, peerIds []int, server *Server, ready <-chan interface{}) *ConsensusModule {
+	cm := new(ConsensusModule)
+	cm.id = id
+	cm.peerIds = peerIds
+	cm.server = server
+	cm.state = Follower
+	cm.votedFor = -1
+
+	go func() {
+		// cm is quet until ready signalled, then start a countdown for leader election
+		<-ready
+		cm.mu.Lock()
+		cm.electionResetEvent = time.Now()
+		cm.mu.Unlock()
+		cm.runElectionTimer()
+	}()
+	return cm
+}
+
+func (cm *ConsensusModule) debugLog(format string, args ...interface{}) {
+	if debugEnable > 0 {
+		format = fmt.Sprintf("[%d] ", cm.id) + format
+		log.Printf(format, args...)
+	}
+}
+
+func (cm *ConsensusModule) Report() (id int, term int, isLeader bool) {
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+	return cm.id, cm.currentTerm, cm.state == Leader
+}
+
+func (cm *ConsensusModule) Stop() {
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+	cm.state = Dead
+	cm.debugLog("id: %d becomes dead", cm.id)
+}
+
+type RequestVoteArgs struct {
+	Term         int
+	CandidateId  int
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+type RequestVoteReply struct {
+	Term        int
+	VoteGranted bool
+}
+
+type AppendEntriesArgs struct {
+	Term     int
+	LeaderId int
+
+	PrevLogIndex int
+	PrevLogTerm  int
+	Entries      []LogEntry
+	LeaderCommit int
+}
+
+type AppendEntriesReply struct {
+	Term    int
+	Success bool
 }
