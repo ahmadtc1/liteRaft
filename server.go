6765847411ffe00bb1e52986fa6b28ea24d4fb7c
ahmadtc1 <ahmadtc2000@gmail.com>
2021-04-20 01:03:52 -0400

    Adding server methods to handle RPC and server methods for our Raft implementation

---
+++ server.go
@@ -0,0 +1,196 @@
+package liteRaft
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
+
+/*
+const (
+	retainSnapshotCount = 2
+	raftTimeout         = 2 * time.Second
+)
+
+type command struct {
+	Op    string `json:"op,omitempty"`
+	Key   string `json:"key,omitempty"`
+	Value string `json:"value,omitempty"`
+	Addr  string `json:"addr,omitempty"`
+}
+*/
+
+// Server wraps liteRaft.ConsensusModule with an rpc.Server that exposes its methods as RPC endpoints
+type Server struct {
+	mu sync.Mutex
+
+	serverId int
+	peerIds  []int
+
+	cm       *ConsensusModule
+	rpcProxy *RPCProxy
+
+	rpcServer *rpc.Server
+	listener  net.Listener
+
+	peerClients map[int]*rpc.Client
+
+	ready <-chan interface{}
+	quit  chan interface{}
+	wg    sync.WaitGroup
+}
+
+func NewServer(serverId int, peerIds []int, ready <-chan interface{}) *Server {
+	s := new(Server)
+	s.serverId = serverId
+	s.peerIds = peerIds
+	s.peerClients = make(map[int]*rpc.Client)
+	s.ready = ready
+	s.quit = make(chan interface{})
+	return s
+}
+
+func (s *Server) Serve() {
+	s.mu.Lock()
+	s.cm = NewConsensusModule(s.serverId, s.peerIds, s, s.ready)
+
+	// create a new RPC server and reg an RPCProxy to fwd all methods to n.cm
+	s.rpcServer = rpc.NewServer()
+	s.rpcProxy = &RPCProxy{cm: s.cm}
+	s.rpcServer.RegisterName("ConsensusModule", s.rpcProxy)
+
+	var err error
+	s.listener, err = net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("[%v] listening at %s", s.serverId, s.listener.Addr())
+	s.mu.Unlock()
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+
+		for {
+			conn, err := s.listener.Accept()
+			if err != nil {
+				select {
+				case <-s.quit:
+					return
+				default:
+					log.Fatal("accept error:", err)
+				}
+			}
+			s.wg.Add(1)
+			go func() {
+				s.rpcServer.ServeConn(conn)
+				s.wg.Done()
+			}()
+		}
+	}()
+}
+
+// close all client connection to peers for this server
+func (s *Server) DisconnectAll() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for id := range s.peerClients {
+		if s.peerClients[id] != nil {
+			s.peerClients[id].Close()
+			s.peerClients[id] = nil
+		}
+	}
+}
+
+// close the server and wait for it to properly shutdown
+func (s *Server) Shutdown() {
+	s.cm.Stop()
+	close(s.quit)
+	s.listener.Close()
+	s.wg.Wait()
+}
+
+func (s *Server) GetListenerAddr() net.Addr {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.listener.Addr()
+}
+
+func (s *Server) ConnectToPeer(peerId int, addr net.Addr) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.peerClients[peerId] == nil {
+		client, err := rpc.Dial(addr.Network(), addr.String())
+		if err != nil {
+			return err
+		}
+		s.peerClients[peerId] = client
+	}
+	return nil
+}
+
+func (s *Server) DisconnectPeer(peerId int) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.peerClients[peerId] != nil {
+		err := s.peerClients[peerId].Close()
+		s.peerClients[peerId] = nil
+		return err
+	}
+	return nil
+}
+
+func (s *Server) Call(id int, serviceMethod string, args interface{}, reply interface{}) error {
+	s.mu.Lock()
+	peer := s.peerClients[id]
+	s.mu.Unlock()
+
+	if peer == nil {
+		return fmt.Errorf("call client %d after it's closed", id)
+	} else {
+		return peer.Call(serviceMethod, args, reply)
+	}
+}
+
+type RPCProxy struct {
+	cm *ConsensusModule
+}
+
+func (rpp *RPCProxy) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error {
+	if len(os.Getenv("RAFT_UNRELIABLE_RPC")) > 0 {
+		dice := rand.Intn(10)
+		if dice == 9 {
+			rpp.cm.debugLog("drop RequestVote")
+			return fmt.Errorf("RPC Failed")
+		} else if dice == 8 {
+			rpp.cm.debugLog("delay RequestVote")
+			time.Sleep(75 * time.Millisecond)
+		}
+	} else {
+		time.Sleep(time.Duration(1+rand.Intn(5)) * time.Millisecond)
+	}
+	return rpp.cm.RequestVote(args, reply)
+}
+
+func (rpp *RPCProxy) AppendEntries(args AppendEntriesArgs, reply *AppendEntriesReply) error {
+	if len(os.Getenv("RAFT_UNRELIABLE_RPC")) > 0 {
+		dice := rand.Intn(10)
+		if dice == 9 {
+			rpp.cm.debugLog("drop RequestVote")
+			return fmt.Errorf("RPC Failed")
+		} else if dice == 8 {
+			rpp.cm.debugLog("delay RequestVote")
+			time.Sleep(75 * time.Millisecond)
+		}
+	} else {
+		time.Sleep(time.Duration(1+rand.Intn(5)) * time.Millisecond)
+	}
+	return rpp.cm.AppendEntries(args, reply)
+}
